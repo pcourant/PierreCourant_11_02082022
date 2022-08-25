ac0090d95bc1732cb0ab67188a4a6ce912c05119
Pierre COURANT <pcourant.dev@gmail.com>
2022-08-25 10:10:40 +0200

    Add call API to get the data

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,17 +1,15 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 // import PropTypes from 'prop-types';
 import Thumb from './Thumb';
 import styles from './Gallery.module.css';
 
-const Gallery = () => {
+const Gallery = ({ accomodations }) => {
   return (
     <section className={styles.container}>
-      <Thumb title="Titre de la location" />
-      <Thumb title="Titre de la location" />
-      <Thumb title="Titre de la location" />
-      <Thumb title="Titre de la location" />
-      <Thumb title="Titre de la location" />
-      <Thumb title="Titre de la location" />
+      {accomodations.map(({ id, title, cover }) => (
+        <Thumb key={id} title={title} cover={cover} />
+      ))}
     </section>
   );
 };
