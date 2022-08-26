e111028557577166aa05e825b1854c0f960b784a
Pierre COURANT <pcourant.dev@gmail.com>
2022-08-26 10:40:27 +0200

    Add(Home): cover picture for cards

--- src/components/Gallery/Thumb/index.jsx
+++ src/components/Gallery/Thumb/index.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Thumb.module.css';
 
-const Thumb = ({ title }) => {
+const Thumb = ({ title, cover }) => {
   return (
     <article className={styles.container}>
       <div className={styles.gradient}>
+        {cover && <img className={styles.cover} src={cover} alt={title} />}
         <h2 className={styles.locationName}>{title}</h2>
       </div>
     </article>
@@ -15,10 +16,12 @@ const Thumb = ({ title }) => {
 
 Thumb.propTypes = {
   title: PropTypes.string,
+  cover: PropTypes.string,
 };
 
 Thumb.defaultProps = {
   title: '',
+  cover: '',
 };
 
 export default Thumb;
