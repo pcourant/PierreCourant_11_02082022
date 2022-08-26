b0fe8aa9e7f0484ad89d5679e0fda453325930ae
Pierre COURANT <pcourant.dev@gmail.com>
2022-08-26 15:17:52 +0200

    Add(collapse): drop/collapse on click

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,13 +1,19 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable react/no-unescaped-entities */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import styles from './Collapse.module.css';
 
-const Collapse = ({ size, title, paragraphs, isDropped }) => {
+const Collapse = ({ size, title, paragraphs, isByDefaultDropped }) => {
+  const [isDropped, setIsDropped] = useState(isByDefaultDropped);
+
+  function handleChevronClick() {
+    setIsDropped((isDropped) => !isDropped);
+  }
+
   return (
     <article className={`${styles.container} ${styles[`container${size}`]}`}>
       <div className={`${styles.top} ${styles[`top${size}`]}`}>
@@ -15,6 +21,7 @@ const Collapse = ({ size, title, paragraphs, isDropped }) => {
         <FontAwesomeIcon
           className={styles[`chevron${size}`]}
           icon={isDropped ? faChevronUp : faChevronDown}
+          onClick={handleChevronClick}
         />
       </div>
       {isDropped && (
@@ -37,14 +44,14 @@ Collapse.propTypes = {
   size: PropTypes.oneOf([50, 75]).isRequired,
   title: PropTypes.string,
   paragraphs: PropTypes.arrayOf(PropTypes.string),
-  isDropped: PropTypes.bool,
+  isByDefaultDropped: PropTypes.bool,
 };
 
 Collapse.defaultProps = {
   size: 75,
   title: '',
   paragraphs: [],
-  isDropped: false,
+  isByDefaultDropped: false,
 };
 
 export default Collapse;
